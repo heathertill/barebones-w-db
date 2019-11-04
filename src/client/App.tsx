0087b58eae23e52989f7528536c76d2bbf78a11a
Heather Till <heather.till@charter.net>
2019-11-04 16:32:04 -0600

    -setup client

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,35 +1,20 @@
 import * as React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import './scss/app';
+import Hello from './components/public/Hello';
 
-export default class App extends React.Component<IAppProps, IAppState> {
-
-    constructor(props: IAppProps) {
-        super(props);
-
-        this.state = { name: null };
-    }
-
-    async componentWillMount() {
-        let r = await fetch('/api/hello');
-        let name = await r.json();
-        this.setState({ name })
-    }
-
-    render () {
-        return (
+const App: React.SFC<AppProps> = () => {
+    return (
+        <BrowserRouter>
             <main className="container">
-                <h1 className="covalence-blue">Hello {this.state.name}!</h1>
-                <h2></h2>
-            </main>
-        )
-    }
+            <Switch>
+                <Route exact path='/' component={Hello} />
+            </Switch>
+           </main>
+        </BrowserRouter>
+    );
 }
 
-interface IAppProps {
+export default App;
 
-}
-
-interface IAppState {
-    name: string;
-}
+export interface AppProps { }
